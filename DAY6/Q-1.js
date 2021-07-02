085064fd29b2b3e4daddb7818dd6ae4e4d2dc561
Sanjana Gore <66768956+sanjanaa0506@users.noreply.github.com>
2021-07-02 21:32:20 +0530

    Q-1.js submitted

---
+++ DAY6/Q-1.js
@@ -0,0 +1,14 @@
+//1.	Write a JavaScript function to check whether an `input` is an array or not
+//Test Data :
+//console.log(is_array('w3resource'));
+//console.log(is_array([1, 2, 4, 0]));
+//false
+//True
+
+
+function is_array(input)
+{
+    return(Array.isArray(input));
+}
+console.log(is_array('w3resource'));
+console.log(is_array([1, 2, 4, 0]));
