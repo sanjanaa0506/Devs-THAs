61c69c84ce64149e82e2c42619d3e639298e9396
Sanjana Gore <66768956+sanjanaa0506@users.noreply.github.com>
2021-07-16 02:02:22 +0530

    THA-14.js done!!

---
+++ DAY14/THA-14.js
@@ -0,0 +1,28 @@
+document.getElementById("Heyy").addEventListener("click",clickMe);
+    function clickMe()
+    {
+        alert ("People call me SAN_JUNEE");
+    }
+
+document.getElementById("Heyi").addEventListener("click",clickMee);
+    function clickMee()
+    {
+        alert ("FUN_JUNE Nice??");
+    }
+
+document.getElementById("Heya").addEventListener("click",clickMeee);
+    function clickMeee()
+    {
+        alert ("RUSSIA");
+    }
+
+document.getElementById("Heyo").addEventListener("click",clickMeeee);
+    function clickMeeee()
+    {
+        alert ("100... SECRETS!!");
+    }
+
+function clickMey()
+{
+    document.getElementById('Heye').innerHTML=" AYAYYY!!! XD XP";
+}
