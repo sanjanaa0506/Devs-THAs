4435f3d1b072f2eb7c52358981d31b9d23ebf1d8
Sanjana Gore <66768956+sanjanaa0506@users.noreply.github.com>
2021-07-17 03:29:22 +0530

    Hello World with React!!

---
+++ DAY15/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World!!! <br></br>I am Sanjune XP<br></br> It is 3:20 AM of 17th July<br></br>I can sleep Peacefully Toodles!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/sanjanaa0506/Devs-THAs/new/main"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My github account!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
