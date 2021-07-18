f798b04f14e3ccb2a9fa92439ba9b9dcd1fd827f
Sanjana Gore <66768956+sanjanaa0506@users.noreply.github.com>
2021-07-18 21:11:33 +0530

    Create index.js

---
+++ DAY18/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Chess from './App';
+import './style.css';
+
+ReactDOM.render(
+  <div class="container">
+    <Chess />
+    </div>,
+  document.getElementById('root')
+);
