e0575083f8e5b638ab131a72fe5750ccf660e62d
Sanjana Gore <66768956+sanjanaa0506@users.noreply.github.com>
2021-07-20 18:28:44 +0530

    Create App.js

---
+++ DAY19/tha-19/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './Button';
+function App() {
+  return (
+    <div className="App">
+     <Button />
+     <Button />
+     <Button />
+     <Button />
+    </div>
+  );
+}
+
+export default App;
