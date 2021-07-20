5e2fb299ea57fbb35c166310b3f64bcbae9d9e7b
Sanjana Gore <66768956+sanjanaa0506@users.noreply.github.com>
2021-07-20 19:09:13 +0530

    index.js updated!

---
+++ DAY20/day20/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App1 from "./App.jsx";
+import App from "./App.js";
+import "./style.css";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
