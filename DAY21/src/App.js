5016fd3290d713bfc55ab4a6079ea8e9e1b98eaa
Sanjana Gore <66768956+sanjanaa0506@users.noreply.github.com>
2021-07-25 18:16:02 +0530

    editing done!

--- DAY21/src/App.js
+++ DAY21/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import Card from './Card';
 function App() {
+  const [newList,setNewList]=useState([])
+  const [listEmpty,setlistEmpty]=useState(false)
+  const [data,setData]=useState({
+    text:'',
+    amount:''
+  });
+
+
+  const dataChange=(e)=>{
+    var name=e.target.name
+    var  value=e.target.value
+    setData({...data,[name]:value})
+     
+  }
+
+  function onChangeSubmit(){
+    setlistEmpty(true)
+    setNewList([...newList,data])
+    setData({
+      text:'',
+      amount:''
+    })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello I am Sanjune!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Good Night
-        </a>
-      </header>
+    <div className="app">
+       <div className="input_values">
+              <input type="text" value={data.text} placeholder="Item Name" className="text" name="text" onChange={dataChange}/>
+              <input type="number" value={data.amount} placeholder="Calorie Amount" className="number" name="amount" onChange={dataChange}/>
+       </div>
+       <button className="btn" onClick={onChangeSubmit}>Add Item</button>
+      {
+        listEmpty ?
+        newList.map((item,index)=>{
+         return  <Card key={index} id={index} title={item.text} amount={item.amount} list={newList} setNewList={setNewList}/>
+        })
+        : <h1 className="warning">Start Adding Items</h1>
+      }
     </div>
   );
 }
