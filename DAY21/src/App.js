a6610ba5baf4c6252dcb1fdfd9b6dd5e99cc6c08
Sanjana Gore <66768956+sanjanaa0506@users.noreply.github.com>
2021-07-25 18:13:56 +0530

    Done!

---
+++ DAY21/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello I am Sanjune!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Good Night
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
